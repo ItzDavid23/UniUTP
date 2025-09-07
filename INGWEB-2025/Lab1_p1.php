7aa5d5d38a52de73fe104c4ffa8f57137df5baab
ItzDavid23 <@davidgj2303@gmail.com>
2025-09-07 07:01:13 -0500

    Ultimas Actualizaciones y estilo para los dos problemas

--- INGWEB-2025/Lab1_p1.php
+++ INGWEB-2025/Lab1_p1.php
@@ -7,18 +7,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Area y Perimetro de un Circulo</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h2>Calculadora de Área y Perímetro de un Círculo</h2>
-    <form method="post">
-        <label for = "radio">Ingrese el Radio del Círculo: </label>
-        <input type = "number" step = "2" name = "radio" id = "radio" required>
-        <button type = "submit">Calcular</button>
+    <form method="post"> <!--Definición del metodo del formulario a POST-->
+        <label for = "radio">Ingrese el Radio del Círculo: </label> <!--Etiqueta del campo-->
+        <input type = "number" step = "any" name = "radio" id = "radio" required> <!--Campo de insercion-->
+        <button type = "submit">Calcular</button> <!--Buton para subir las entradas-->
     </form>
     <?php
-    if ($_SERVER[REQUEST_METHOD] == "POST"){
-        $radio = $_POST["radio"];
-        if ($radio > 0) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){ //Definición del metodo de solicitud
+        $radio = $_POST["radio"]; //Asignacion de la solicitud a la variable $radio
+        if ($radio > 0) { //Condicion para el calculo, radio debe ser menor que 0
             $pi = pi();
             $area = $pi * $radio * $radio;
             $perimetro = 2 * $pi * $radio;
@@ -28,7 +29,7 @@
             echo "Área: " . round($area, 2) . "<br>";
             echo "Perímetro: " . round($perimetro, 2) . "<br>";
         } else {
-            echo "<p style='color:red;'>El radio debe ser mayor que 0.</p>";
+            echo "<p style='color:red;'>El radio debe ser mayor que 0.</p>"; //Condicion sino si el numero es menor a 0
         }
 }
 ?>
--- INGWEB-2025/Lab1_p2.php
+++ INGWEB-2025/Lab1_p2.php
@@ -4,22 +4,23 @@
     <meta charset = 'UTF-8'>
     <meta name = 'viewport' content = 'width-devide-width, initial-scale=1.0'>
     <title>Pulgadas a Centimetros</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h2>Calculadora de Pulgadas a Centimetros<h/>
-    <form method='POST'>
-        <label for="pulgadas" name='Lpulg' id='L1' for='Pulgadas'>Ingrese las pulgadas</label>
-        <input type='number' step='2' name=Ipulg id='I2' required>
+    <h2>Calculadora de Pulgadas a Centimetros</h2>
+    <form method="POST">
+        <label for="pulgadas" name='Lpulg' id='L1' for='Pulgadas'>Ingrese las pulgadas: </label>
+        <input type='number' step='any' name="pulgadas" id='I2' required>
         <button type='submit' name='bcalcular' id=B1>Calcular</button>
     </form>
     <?php
-    if ($_SERVER[REQUEST_METHOD] == "POST"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $pulgada=$_POST['pulgadas'];
-        if($pulgada > 0){
+        if ($pulgada > 0){
             $centimetro = $pulgada * 2.54;
-            echo 'Las pulgadas ' . $pulgada . 'En centimetros:';
-            echo $pulgada . 'cm';
+            echo $pulgada . " Pulgadas en centimetro son: <br>";
+            echo $centimetro . 'cm';
         }
         else{
             echo "<p style='color:red;'>No puede ser menor que 0 las pulgadas</p>";
